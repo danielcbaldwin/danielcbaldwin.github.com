d6ceed145f0968791ea0b37617ccfff8465a8909
Daniel Baldwin <danielcbaldwin@gmail.com>
2011-08-22 21:54:54 -0600

    removed gist cache

--- _plugins/gist_tag.rb
+++ _plugins/gist_tag.rb
@@ -8,7 +8,7 @@ module Jekyll
     def initialize(tag_name, text, token)
       super
       @text           = text
-      @cache_disabled = false
+      @cache_disabled = true
       @cache_folder   = File.expand_path "../_gist_cache", File.dirname(__FILE__)
     end
 
