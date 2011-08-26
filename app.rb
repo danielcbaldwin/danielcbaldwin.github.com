0b829ac5924c1c0db68e2d3d647fe0cfb3f648ff
Daniel Baldwin <danielcbaldwin@gmail.com>
2011-08-25 23:49:45 -0600

    added default

--- app.rb
+++ app.rb
@@ -2,21 +2,24 @@ require 'sinatra'
 require 'RMagick'
 
 set :static, true
-set :public, "./_site"
+set :public, Proc.new {File.join(root, '_site')}
 
 imagedir = "./_site/img"
+
+before do
+  response.headers['Cache-Control'] = 'public, max-age=31557600' # 1 year
+end
 
 not_found do
 	errorFile='_site/404/index.html'
 	File.read(errorFile)
 end
 
-get "/hi" do 
-	"Hello World"
+get '/' do
+  File.read(File.join('public', 'index.html'))
 end
 
 get "/image/*/to/*" do
-  response['Cache-Control'] = "public, max-age=31556926"
 	if FileTest.exists?(imagedir + "/" + params[:splat][0])
 		im = Magick::ImageList.new(imagedir + "/" + params[:splat][0])
 		content_type im.mime_type
