78dcb25ca79cfb456a378ec03f26dfffb46cf525
Daniel Baldwin <danielcbaldwin@gmail.com>
2011-08-26 00:09:41 -0600

    restoring

--- app.rb
+++ app.rb
@@ -14,24 +14,24 @@ before do
 end
 
 not_found do
-	errorFile='#{static_path}/404/index.html'
+	errorFile = "#{static_path}/404/index.html"
 	File.read(errorFile)
 end
 
-get '/' do
-  File.read(File.join(static_path, 'index.html'))
-end
-
-get '/*' do 
-  path = File.join(static_path, params[:splat])
-  if FileTest.exists?(path) && !File.directory?(path)
-    File.read(path)
-  elseif File.directory?(path) && FileTest.exists?(File.join(path, 'index.html'))
-    File.read(File.join(path, 'index.html'))
-  else
-    raise not_found
-  end
-end
+# get '/' do
+#   File.read(File.join(static_path, 'index.html'))
+# end
+# 
+# get '/*' do 
+#   path = File.join(static_path, params[:splat])
+#   if FileTest.exists?(path) && !File.directory?(path)
+#     File.read(path)
+#   elseif File.directory?(path) && FileTest.exists?(File.join(path, 'index.html'))
+#     File.read(File.join(path, 'index.html'))
+#   else
+#     raise not_found
+#   end
+# end
 
 get "/image/*/to/*" do
 	if FileTest.exists?(imagedir + "/" + params[:splat][0])
